a8ca2e944492e1af312651148d4527cf0858d1c2
spacemeowx2 <spacemeowx2@gmail.com>
2019-07-29 00:34:20 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+extern crate rawsock;
+
 mod shit;
 mod rawsock_interface;
+mod get_mac;
+use get_mac::GetMac;
 use self::shit::{Shit, SthTrait};
+use rawsock_interface::RawsockInterface;
+use rawsock::{open_best_library};
+
+fn fuck() {
+    println!("Opening packet capturing library");
+    let lib = open_best_library().expect("Could not open any packet capturing library");
+    println!("Library opened, version is {}", lib.version());
+    let all_interf = lib.all_interfaces().expect("Could not obtain interface list");
+    for interf in all_interf {
+        let name = interf.name;
+        println!("Opening the {} interface", &name);
+        let interf = lib.open_interface(&name).expect("Could not open network interface");
+        println!("Interface opened, data link: {}", interf.data_link());
+    }
+}
 
 fn main() {
     let a = Shit::new();
+    RawsockInterface::new();
     println!("Hello, world! {}", a.a);
+    fuck();
 }
--- src/rawsock_interface.rs
+++ src/rawsock_interface.rs
@@ -1,17 +1,28 @@
 extern crate smoltcp;
+extern crate rawsock;
 use smoltcp::phy::{Device,DeviceCapabilities,RxToken,TxToken};
 use smoltcp::time::Instant;
 use smoltcp::Result;
 
 pub struct RawsockInterface {
-
+    rx_buffer: [u8; 1536],
+    tx_buffer: [u8; 1536],
 }
 
-struct RawRxToken<'a>(&'a mut [u8]);
+pub struct RawRxToken<'a>(&'a mut [u8]);
+
+impl RawsockInterface {
+    pub fn new() -> RawsockInterface {
+        RawsockInterface {
+            rx_buffer: [0; 1536],
+            tx_buffer: [0; 1536],
+        }
+    }
+}
 
 impl<'a> RxToken for RawRxToken<'a> {
     fn consume<R, F>(mut self, _timestamp: Instant, f: F) -> Result<R>
-        where F: FnOnce(&mut [u8]) -> Result<R>
+        where F: FnOnce(&[u8]) -> Result<R>
     {
         // TODO: receive packet into buffer
         let result = f(&mut self.0);
@@ -21,7 +32,7 @@ impl<'a> RxToken for RawRxToken<'a> {
 }
 
 
-struct RawTxToken<'a>(&'a mut [u8]);
+pub struct RawTxToken<'a>(&'a mut [u8]);
 
 impl<'a> TxToken for RawTxToken<'a> {
     fn consume<R, F>(self, _timestamp: Instant, len: usize, f: F) -> Result<R>
@@ -38,12 +49,13 @@ impl<'a> Device<'a> for RawsockInterface {
     type RxToken = RawRxToken<'a>;
     type TxToken = RawTxToken<'a>;
 
-    fn receive(&mut self) -> Option<(Self::RxToken, Self::TxToken)> {
-        Ok()
+    fn receive(&'a mut self) -> Option<(Self::RxToken, Self::TxToken)> {
+        Some((RawRxToken(&mut self.rx_buffer[..]),
+              RawTxToken(&mut self.tx_buffer[..])))
     }
 
-    fn transmit(&mut self) -> Option<Self::TxToken> {
-
+    fn transmit(&'a mut self) -> Option<Self::TxToken> {
+        Some(RawTxToken(&mut self.tx_buffer[..]))
     }
 
     fn capabilities(&self) -> DeviceCapabilities {
