04928549a039f3f5e991beef0cec0fdbe8b8b0a5
spacemeowx2 <spacemeowx2@gmail.com>
2019-07-29 21:29:23 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,37 @@ mod get_mac;
 use get_mac::{GetMac};
 use self::shit::{Shit, SthTrait};
 use rawsock_interface::RawsockInterface;
-use rawsock::{open_best_library};
+use rawsock::{DataLink, open_best_library};
 
 fn main() {
     let a = Shit::new();
-    RawsockInterface::new();
     println!("Hello, world! {}", a.a);
 
     println!("Opening packet capturing library");
     let lib = open_best_library().expect("Could not open any packet capturing library");
     println!("Library opened, version is {}", lib.version());
     let all_interf = lib.all_interfaces().expect("Could not obtain interface list");
-    let fuck = all_interf.into_iter().map(|i| {
+    let (opened, errored): (_,Vec<_>) = all_interf.into_iter().map(|i| {
         let name = i.name;
         println!("Opening the {} interface", &name);
         // Some(lib.open_interface(&name).expect("Could not open network interface"))
         lib.open_interface(&name).ok()
-    }).filter(|oi| {
+    }).partition(|oi| {
         match oi {
             Some(i) => match i.data_link() {
-                rawsock::DataLink::Ethernet => true,
+                DataLink::Ethernet => true,
                 _ => false
             },
             None => false
         }
-    }).map(|i| -> Box<dyn rawsock::traits::Interface> {
-        i.unwrap()
     });
-    for mut interf in fuck {
+    let fuck = opened.into_iter().map(|i| {
+        RawsockInterface::new(i.unwrap())
+    });
+    for mut interface in fuck {
+        let interf = &mut interface.interface;
         let mac = interf.get_mac().expect("Could not get mac");
-        println!("Interface opened, data link: {}, mac: {}", interf.data_link(), mac);
+        println!("Interface {} opened, data link: {}, mac: {}", interf., interf.data_link(), mac);
         let p = interf.receive();
     }
 }
--- src/rawsock_interface.rs
+++ src/rawsock_interface.rs
@@ -3,20 +3,75 @@ extern crate rawsock;
 use smoltcp::phy::{Device,DeviceCapabilities,RxToken,TxToken};
 use smoltcp::time::Instant;
 use smoltcp::Result;
+use smoltcp::socket::{TcpSocket, TcpState};
+use smoltcp::wire::{IpRepr, TcpRepr};
+use std::sync::mpsc;
+use std::thread;
+use std::thread::{JoinHandle};
+use rawsock::traits::{Interface, Library};
+use rawsock::InterfaceDescription;
 
-pub struct RawsockInterface {
+pub struct RawsockInterface<'a> {
     rx_buffer: [u8; 1536],
     tx_buffer: [u8; 1536],
+    thread: JoinHandle<()>,
+    pub interface: Box<dyn Interface<'a> + 'a>,
+    pub desc: InterfaceDescription,
+    stopper: std::sync::mpsc::Sender<()>
 }
 
 pub struct RawRxToken<'a>(&'a mut [u8]);
 
-impl RawsockInterface {
-    pub fn new() -> RawsockInterface {
-        RawsockInterface {
-            rx_buffer: [0; 1536],
-            tx_buffer: [0; 1536],
-        }
+pub trait Fuck {
+    fn accepts(&self, ip_repr: &IpRepr, repr: &TcpRepr) -> bool;
+}
+
+impl<'a> Fuck for TcpSocket<'a> {
+    fn accepts(&self, ip_repr: &IpRepr, repr: &TcpRepr) -> bool {
+        if self.state() == TcpState::Closed { return false }
+
+        // If we're still listening for SYNs and the packet has an ACK, it cannot
+        // be destined to this socket, but another one may well listen on the same
+        // local endpoint.
+        if self.state() == TcpState::Listen && repr.ack_number.is_some() { return false }
+
+        // Reject packets with a wrong destination.
+        if self.local_endpoint().port != repr.dst_port { return false }
+        if !self.local_endpoint().addr.is_unspecified() &&
+            self.local_endpoint().addr != ip_repr.dst_addr() { return false }
+
+        // Reject packets from a source to which we aren't connected.
+        if self.remote_endpoint().port != 0 &&
+            self.remote_endpoint().port != repr.src_port { return false }
+        if !self.remote_endpoint().addr.is_unspecified() &&
+            self.remote_endpoint().addr != ip_repr.src_addr() { return false }
+
+        true
+    }
+}
+
+impl<'a> RawsockInterface<'a> {
+    // pub fn new(desc: InterfaceDescription) -> RawsockInterface<'a> {
+    //     let (tx, rx) = mpsc::channel::<()>();
+    //     let thread = thread::spawn(move || {
+            
+    //     });
+    //     let interface = 
+    //     RawsockInterface {
+    //         rx_buffer: [0; 1536],
+    //         tx_buffer: [0; 1536],
+    //         thread,
+    //         interface: ,
+    //         desc,
+    //         stopper: tx
+    //     }
+    // }
+}
+
+impl<'a> Drop for RawsockInterface<'a> {
+    fn drop(&mut self) {
+        // self.stopper.send(()).unwrap();
+        // self.thread.join();
     }
 }
 
@@ -45,7 +100,7 @@ impl<'a> TxToken for RawTxToken<'a> {
     }
 }
 
-impl<'a> Device<'a> for RawsockInterface {
+impl<'a> Device<'a> for RawsockInterface<'a> {
     type RxToken = RawRxToken<'a>;
     type TxToken = RawTxToken<'a>;
 
